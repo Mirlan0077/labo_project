aeac58a991086fe94c9cdc5c99ab1922e95ce805
Mirlan0077 <mirlan.0077m@gmail.com>
2024-07-04 14:19:30 +0200

    Добавлено перенаправление с главной страницы на список категорий

--- shop_project/urls.py
+++ shop_project/urls.py
@@ -1,9 +1,10 @@
 
-
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
+    path('', RedirectView.as_view(url='/shop/categories/', permanent=True)),
 ]
